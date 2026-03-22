c1e49835ec94f1706c49ed586fe44b6aa2991659
Anand <slokeshvarma117@gmail.com>
2026-03-22 10:33:31 +0530

    navStickyMobile Complete

--- main.js
+++ main.js
@@ -1,44 +1,3 @@
-function navStickyFunctionDesktop() {
-    const collgeLabelHeight = document.getElementById("collegeLabel").clientHeight;
-    const nav = document.getElementsByTagName("nav")[0];
-    const navLogo = document.getElementsByClassName("navLogo")[0];
-    const pageScrollValue = window.scrollY || document.documentElement.scrollTop;
-    const root = document.documentElement;
-    const style = getComputedStyle(root);
-    const navLinkPadding = parseFloat(style.getPropertyValue('--navLinkPadding'));
-    const primaryColor = style.getPropertyValue('--primaryColor');
-    const navLinks = document.getElementsByClassName("navLink");
-
-    if (pageScrollValue >= collgeLabelHeight) {
-        navLogo.style.display = `flex`;
-        nav.style.position = "fixed";
-        nav.style.top = 0;
-        nav.style.backgroundColor = "white";
-        nav.style.paddingTop = `1rem`;
-        nav.style.paddingBottom = `1rem`;
-        Array.from(navLinks).forEach(navLink => {
-            navLink.style.color = primaryColor;
-            navLink.style.padding = `${0.085*navLinkPadding}px ${0.25*navLinkPadding}px`;
-        });
-    } else {
-        const scrollRatio = pageScrollValue/collgeLabelHeight;
-        navLogo.style.display = `none`;
-        nav.style.position = "relative";
-        nav.style.top = collgeLabelHeight;
-        const paddingTopBottom = 0.5 + 0.5*scrollRatio;
-        nav.style.paddingTop = `${paddingTopBottom}rem`;
-        nav.style.paddingBottom = `${paddingTopBottom}rem`;
-        Array.from(navLinks).forEach(navLink => {
-            navLink.style.padding = `${0.15*navLinkPadding}px ${0.5*navLinkPadding}px`;
-        });
-        if (scrollRatio >= 0.75) {
-            navLogo.style.display = `flex`;
-            navLogo.style.transform = `scale(${scrollRatio})`;
-        }
-    }
-};
-
-
 function navMobileMenu() {
     const navLinks = document.getElementsByClassName("navLinks")[0];
     const navHamburgerLines = document.getElementsByClassName("navHamburgerLine");
@@ -50,8 +9,11 @@ function navMobileMenu() {
 
 function navSticky() {
     const pageScrollValue = window.scrollY || document.documentElement.scrollTop;
-    console.log(pageScrollValue);
-
+    const collgeLabelHeight = document.getElementsByClassName("collegeLabel")[0].clientHeight;
+    let pageScrolledPassed = false;
+    if (pageScrollValue/collgeLabelHeight >= 1) {
+        pageScrolledPassed = true;
+    }
     const nav = document.getElementsByTagName("nav")[0];
     const navLogo = document.getElementsByClassName("navLogo")[0];
     const navImage = document.getElementsByClassName("navImage")[0];
@@ -61,14 +23,14 @@ function navSticky() {
     const landingSection = document.getElementsByClassName("landingSection")[0];
 
     Array.from(navHamburgerLines).forEach(hamburgerLine => {
-        hamburgerLine.classList.toggle("sticked");
+        hamburgerLine.classList.toggle("sticked", pageScrolledPassed);
     })
-    nav.classList.toggle("sticked");
-    navLogo.classList.toggle("sticked");
-    navImage.classList.toggle("sticked");
-    navTitle.classList.toggle("sticked");
-    navLinksdiv.classList.toggle("sticked");
-    landingSection.classList.toggle("sticked");
+    nav.classList.toggle("sticked", pageScrolledPassed);
+    navLogo.classList.toggle("sticked", pageScrolledPassed);
+    navImage.classList.toggle("sticked", pageScrolledPassed);
+    navTitle.classList.toggle("sticked", pageScrolledPassed);
+    navLinksdiv.classList.toggle("sticked", pageScrolledPassed);
+    landingSection.classList.toggle("sticked", pageScrolledPassed);
 }
 
 function toggleTheme() {
@@ -76,10 +38,5 @@ function toggleTheme() {
   document.documentElement.classList.toggle('darkMode');
 }
 
-function nameIT() {
-    const scrollY = window.pageYOffset || document.documentElement.scrollTop;
-    console.log("Mobile scroll:", scrollY);
-}
-
-window.addEventListener('scroll', nameIT, { passive: true });
-window.addEventListener('touchmove', () => console.log("Touch scroll!")); // Mobile bonus
+window.addEventListener('scroll', navSticky, { passive: true });
+window.addEventListener('touchmove', navSticky);
