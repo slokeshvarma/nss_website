178a26553fb7d5b8c2c60376065ac492f691dfa9
Anand <slokeshvarma117@gmail.com>
2026-03-22 14:31:15 +0530

    Navigation Bar Complete for all Devices

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 function navMobileMenu() {
+    const navHeight = document.getElementsByTagName("nav")[0].clientHeight;
     const navLinks = document.getElementsByClassName("navLinks")[0];
     const navHamburgerLines = document.getElementsByClassName("navHamburgerLine");
+
     Array.from(navHamburgerLines).forEach(hamburgerLine => {
         hamburgerLine.classList.toggle("clicked");
     }) 
     navLinks.classList.toggle("clicked");
+    if (navLinks.classList.contains("clicked")) {
+        navLinks.style.top = (navHeight - 2) + "px";
+    } else {
+        navLinks.style.top = "";    
+  }
 }
 
 function navSticky() {
@@ -19,9 +26,12 @@ function navSticky() {
     const navImage = document.getElementsByClassName("navImage")[0];
     const navTitle = document.getElementsByClassName("navTitle")[0];
     const navLinksdiv = document.getElementsByClassName("navLinks")[0];
+    const navLinks = document.getElementsByClassName("navLink");
     const navHamburgerLines = document.getElementsByClassName("navHamburgerLine");
     const landingSection = document.getElementsByClassName("landingSection")[0];
-
+    Array.from(navLinks).forEach(navLink => {
+        navLink.classList.toggle("clicked", pageScrolledPassed);
+    })
     Array.from(navHamburgerLines).forEach(hamburgerLine => {
         hamburgerLine.classList.toggle("sticked", pageScrolledPassed);
     })
@@ -40,3 +50,14 @@ function toggleTheme() {
 
 window.addEventListener('scroll', navSticky, { passive: true });
 window.addEventListener('touchmove', navSticky);
+
+function openLink(link) {
+  window.open(link, "_blank");
+}
+const hamburgerMenu = document.getElementById("navHamburgerClickable");
+const GVPLogo = document.getElementById("collegeLabelGVPLogo");
+const NSSLogo = document.getElementById("collegeLabelNSSLogo");
+
+hamburgerMenu.addEventListener("click", navMobileMenu);
+GVPLogo.addEventListener("click", () => openLink("https://www.gvpce.ac.in/"));
+NSSLogo.addEventListener("click", () => openLink("https://nss.gov.in/"));
