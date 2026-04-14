5757f2841c9f025047f67c3e4e0a192f3090c5cc
Anand <slokeshvarma117@gmail.com>
2026-04-14 10:43:53 +0530

    HomeGalleryInitiation

--- main.js
+++ main.js
@@ -1,8 +1,17 @@
+/* functions */
+function toggleTheme() {
+    document.documentElement.classList.toggle('lightMode');
+    document.documentElement.classList.toggle('darkMode');
+};
+
+function openLink(link) {
+  window.open(link, "_blank");
+};
+
 function navMobileMenu() {
     const navHeight = document.getElementsByTagName("nav")[0].clientHeight;
     const navLinks = document.getElementsByClassName("navLinks")[0];
     const navHamburgerLines = document.getElementsByClassName("navHamburgerLine");
-
     Array.from(navHamburgerLines).forEach(hamburgerLine => {
         hamburgerLine.classList.toggle("clicked");
     }) 
@@ -12,90 +21,133 @@ function navMobileMenu() {
     } else {
         navLinks.style.top = "";    
   }
-}
+};
 
-function navSticky(pageScrolledPassed) {
-    const nav = document.getElementsByTagName("nav")[0];
-    const navLogo = document.getElementsByClassName("navLogo")[0];
-    const navImage = document.getElementsByClassName("navImage")[0];
-    const navTitle = document.getElementsByClassName("navTitle")[0];
-    const navLinksdiv = document.getElementsByClassName("navLinks")[0];
-    const navLinks = document.getElementsByClassName("navLink");
-    const navHamburgerLines = document.getElementsByClassName("navHamburgerLine");
+function navSticky() {
+    const pageScrollValue = window.scrollY || document.documentElement.scrollTop;
+    const collgeLabelHeight = document.getElementsByClassName("collegeLabel")[0].clientHeight;
+    const root = document.documentElement;
+    const pageScrolledTill = pageScrollValue/collgeLabelHeight;
+    let pageScrolledPassed = false;
+    if (pageScrolledTill >= 1) {
+        pageScrolledPassed = true;
+        root.style.setProperty("--pageScrolledTill", Math.round(pageScrolledTill * 100)/100);
+        if (pageScrolledTill >= 1.5) {
+            root.style.setProperty("--pageScrolledTill", 1.5);
+        }
+    }
+    const header = document.getElementsByTagName("header")[0];
     const landingSection = document.getElementsByClassName("landingSection")[0];
     const aboutUs = document.getElementById("aboutUs");
-    const aboutUshrs = document.getElementsByClassName("aboutUshr");
-
-    Array.from(navLinks).forEach(navLink => {
-        navLink.classList.toggle("sticked", pageScrolledPassed);
-    })
-    Array.from(aboutUshrs).forEach(aboutUshr => {
-        aboutUshr.classList.toggle("sticked", pageScrolledPassed);
-    })
-    Array.from(navHamburgerLines).forEach(hamburgerLine => {
-        hamburgerLine.classList.toggle("sticked", pageScrolledPassed);
-    })
-    nav.classList.toggle("sticked", pageScrolledPassed);
-    navLogo.classList.toggle("sticked", pageScrolledPassed);
-    navImage.classList.toggle("sticked", pageScrolledPassed);
-    navTitle.classList.toggle("sticked", pageScrolledPassed);
-    navLinksdiv.classList.toggle("sticked", pageScrolledPassed);
+    header.classList.toggle("sticked", pageScrolledPassed);
     landingSection.classList.toggle("sticked", pageScrolledPassed);
     aboutUs.classList.toggle("sticked", pageScrolledPassed);
-}
+};
 
-function toggleTheme() {
-    document.documentElement.classList.toggle('lightMode');
-    document.documentElement.classList.toggle('darkMode');
-}
+function loadCSV(path) {
+    return new Promise((resolve, reject) => {
+        Papa.parse(path, {download: true, header: true, complete: results => resolve(results.data), error: err => reject(err) });
+    });
+};
 
-window.addEventListener('scroll', 
-    () => {
-        const pageScrollValue = window.scrollY || document.documentElement.scrollTop;
-        const collgeLabelHeight = document.getElementsByClassName("collegeLabel")[0].clientHeight;
-        const nav = document.getElementsByTagName("nav")[0];
-        const root = document.documentElement;
-        root.style.setProperty("--navHeight", nav.offsetHeight + "px");
+function renderHomeGallery(data) {
+    if (initialHomeGalleryRender) {
+        let count = 1;
+        const homeGalleryImages16_9 = document.getElementById("homeGalleryImages16-9");
+        const homeGalleryImages4_3 = document.getElementById("homeGalleryImages4-3");
 
-        let pageScrolledPassed = false;
-        if (pageScrollValue/collgeLabelHeight >= 1) {
-            pageScrolledPassed = true;
-            root.style.setProperty("--pageScrolledTill", Math.round((pageScrollValue/collgeLabelHeight)*100)/100);
-            if (pageScrollValue/collgeLabelHeight >= 2) {
-                root.style.setProperty("--pageScrolledTill", 2);
-            }
-        }
-        if (!pageScrolledPassed) {
-        }
-        navSticky(pageScrolledPassed);
+        data.forEach(row => {
+            image_16_9 = document.createElement("img");
+            image_16_9.className = `${count} homeGalleryImage16-9`;
+            image_16_9.src = `${row.nonMobileImageLink}`;
+            homeGalleryImages16_9.appendChild(image_16_9);
 
-}, { passive: true });
-window.addEventListener('touchmove', navSticky);
+            image_4_3 = document.createElement("img");
+            image_4_3.className = `${count} homeGalleryImage4_3`;
+            image_4_3.src = `${row.mobileImageLink}`;
+            homeGalleryImages4_3.appendChild(image_4_3);
 
-function openLink(link) {
-  window.open(link, "_blank");
-}
-const hamburgerMenu = document.getElementById("navHamburgerClickable");
+            count++;
+        })
+        initialHomeGalleryRender = false;
+    }
+};
+
+function currentImageWraper(div, currentImage) {
+    const totalImagesCount = div.children.length;
+    if (currentImage < 1){
+        currentImageNumber = totalImagesCount;
+    } else if (currentImage > totalImagesCount){
+        currentImageNumber = 1;
+    } else {
+        currentImageNumber = currentImage;
+    }
+};
+
+function currentImageDiv() {
+    let imageDiv;
+    const homeGalleryImages16_9 = document.getElementById("homeGalleryImages16-9");
+    const homeGalleryImages4_3 = document.getElementById("homeGalleryImages4-3");
+    if (homeGalleryImages16_9.style.display === "none") {
+        imageDiv = homeGalleryImages4_3;
+    } else if (homeGalleryImages4_3.style.display === "none") {
+        imageDiv = homeGalleryImages16_9;
+    }
+    return imageDiv;
+};
+
+function homeImageGallery(currentImage) {
+    currentImageWraper(currentImageDiv(), currentImage);
+};
+
+function renderHomeEvents(data) {
+    if (true) {
+        console.log(data);
+
+    }
+};
+
+/* execution of functions */
+/* link to the images in header */
 const GVPLogo = document.getElementById("collegeLabelGVPLogo");
 const NSSLogo = document.getElementById("collegeLabelNSSLogo");
 
-hamburgerMenu.addEventListener("click", navMobileMenu);
 GVPLogo.addEventListener("click", () => openLink("https://www.gvpce.ac.in/"));
 NSSLogo.addEventListener("click", () => openLink("https://nss.gov.in/"));
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener("click", function(event) {
-        event.preventDefault();
+/* nav bar's total function integration */
+const nav = document.getElementsByTagName("nav")[0];
+const hamburgerMenu = document.getElementById("navHamburgerClickable");
 
-        let targetID = this.getAttribute("href");
-        let targetSection = document.querySelector(targetID);
-        let navHeight = document.querySelector("nav").offsetHeight;
-        console.log(targetSection.offsetTop - navHeight)
-        if (targetSection) {
-            window.scrollTo({
-                top: targetSection.offsetTop - navHeight,
-                behavior: "smooth"
-            });
+window.addEventListener('scroll', navSticky, { passive: true });
+window.addEventListener('touchmove', navSticky);
+hamburgerMenu.addEventListener('click', navMobileMenu);
+
+document.documentElement.style.setProperty("--navHeight", nav.offsetHeight + "px");
+
+/* csv data loading */
+let homeGalleryData = [];
+let homeEventsData = [];
+let initialHomeGalleryRender = true;
+var currentImageNumber = 1;
+
+loadCSV("dataTables/homePageData.csv").then(data => {    
+    data.forEach(row => {
+        if (row.section === "homeGallery") {
+            homeGalleryData.push({
+                mobileImageLink: row.eventMobileImageLink,
+                nonMobileImageLink: row.eventNonMobileImageLink,
+            })
+        } else if (row.section === "homeEvents") {
+            homeEventsData.push({
+                eventName: row.eventName,
+                eventDate: row.eventDate,
+                eventUnit: row.eventUnit,
+                eventDescription: row.eventDescription,
+                eventPosterLink: row.eventPosterLink
+            })
         }
     });
+    renderHomeGallery(homeGalleryData);
+    renderHomeEvents(homeEventsData);
 });
